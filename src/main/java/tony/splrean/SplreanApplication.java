511a44f728deec1db7be5ca3bbf038f81d9cfa54
mins1031 <urisegea@naver.com>
2025-08-04 00:18:29 +0900

    첫 커밋

---
+++ src/main/java/tony/splrean/SplreanApplication.java
@@ -0,0 +1,13 @@
+package tony.splrean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplreanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplreanApplication.class, args);
+    }
+
+}
